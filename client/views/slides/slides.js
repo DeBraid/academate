2a89774385be01578a024cc9d15a5177c666686c
DeBraid <de.braid@gmail.com>
2014-10-01 20:50:04 -0400

    added slides with some style, and using helper dummy

--- client/views/slides/slides.js
+++ client/views/slides/slides.js
@@ -1,20 +1,13 @@
 Template.slides.rendered = function () {
-
-  Reveal.initialize({
-    // The "normal" size of the presentation, aspect ratio will be preserved
-    // when the presentation is scaled to fit different resolutions. Can be
-    // specified using percentage units.
-    width: 960,
-    height: 700,
-
-    // Factor of the display size that should remain empty around the content
-    margin: 0.1,
-
-    // Bounds for smallest/largest possible scale to apply to content
-    minScale: 1,
-    maxScale: 5.0
-
-});
-  Reveal.slide();
-
+  $("[data-toggle=tooltip]").tooltip();
 };
+
+Template.slides.helpers({
+  sections: function () {
+    return [
+      {baz : 'test1'},
+      {baz : 'test two'},
+      {baz : 'test three'},
+    ]
+  }
+});
